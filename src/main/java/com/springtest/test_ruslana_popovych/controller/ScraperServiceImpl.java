2961e7e72461d2629e9211f4e949a9617f08757c
Ruslana478 <ruslanapopovych2004@gmail.com>
2022-12-23 12:37:02 +0200

    Initial commit

---
+++ src/main/java/com/springtest/test_ruslana_popovych/controller/ScraperServiceImpl.java
@@ -0,0 +1,49 @@
+package com.springtest.test_ruslana_popovych.controller;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class ScraperServiceImpl implements ScraperService {
+    //Reading data from property file to a list
+    String url="https://cex.io/";
+
+    @Override
+    public Set<ResponseDTO> getCrypto(String vehicleModel) {
+        Set<ResponseDTO> responseDTOS = new HashSet<>();
+        if (url.contains("cex")) {
+            //method to extract data from Ikman.lk
+            extractDataFromCex(responseDTOS, url + vehicleModel);
+        }
+        return responseDTOS;
+    }
+    private void extractDataFromCex(Set<ResponseDTO> responseDTOS, String url) {
+
+        try {            //loading the HTML to a Document Object
+            Document document = Jsoup.connect(url).get();            //Selecting the element which contains the ad list
+            Element element = document.getElementsByClass("cex-ui-section-content").first();
+            //getting all the <a> tag elements inside the content div tag
+            Elements elements = element.getElementsByTag("tr");
+            //traversing through the elements
+            for (Element e: elements) {
+                ResponseDTO responseDTO = new ResponseDTO();
+
+                if (!StringUtils.isEmpty(e.attr("title")) ) {                    //mapping data to the model class
+                    responseDTO.setCurrencyName(e.attr("td"));
+                    responseDTO.setPrice(Double.parseDouble(e.attr("td")));
+                }
+                if (responseDTO.getPrice() != 0) responseDTOS.add(responseDTO);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/springtest/test_ruslana_popovych/TestRuslanaPopovychApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtest.test_ruslana_popovych;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRuslanaPopovychApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
